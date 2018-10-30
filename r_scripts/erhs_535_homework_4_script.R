deaad9681689cd95b3f5dd4e3e36e8606a32c80b
Kyle S. Root <kroot27@gmail.com>
2018-10-30 11:05:00 -0600

    ran a test of proportions for baltimore

--- r_scripts/erhs_535_homework_4_script.R
+++ r_scripts/erhs_535_homework_4_script.R
@@ -8,20 +8,18 @@ homicides <- homicides %>%
   unite(city_name, city, state, sep = ", ")
 
 homicides$city_name
-  
+
+unique(homicides$disposition)
+
 unsolved <- homicides %>% 
   mutate(unsolved = disposition == "Closed without arrest" | disposition == "Open/No arrest") %>% 
   group_by(city_name) %>% 
   summarize(n_homicides = n(),
             n_unsolved = sum(unsolved))
 
+baltimore <- unsolved %>% 
+  filter(city_name == "Baltimore, MD")
 
+prop.test(x = baltimore$n_unsolved,
+          n = baltimore$n_homicides)
 
-levels(homicides$disposition)
-
-
-  
-  
-  
-
-  group_by(city_name)
