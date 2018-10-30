a6204d7bf7dc6b4ad29d719f90999bacf599e6f1
Kyle S. Root <kroot27@gmail.com>
2018-10-30 08:34:43 -0600

    Moved data towards requested summaries

--- r_scripts/erhs_535_homework_4_script.R
+++ r_scripts/erhs_535_homework_4_script.R
@@ -3,3 +3,24 @@ library(lubridate)
 
 homicides <- read_csv("data_raw/data-homicides-master/homicide-data.csv")
 head(homicides)
+
+homicides <- homicides %>% 
+  unite(city_name, city, state, sep = ", ")
+
+homicides$city_name
+
+homicides %>% 
+  mutate(unsolved = disposition == "Closed without arrest" | disposition == "Open/No arrest") %>% 
+  group_by(city_name) %>% 
+  summarize(n_homicides = n(),
+            n_unsolved = count(unsolved))
+  
+  
+levels(homicides$disposition)
+
+
+  
+  
+  
+
+  group_by(city_name)
