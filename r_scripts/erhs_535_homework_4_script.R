c5d24e9b60800f5e97ea85362bdc8f1e711daeb4
Kyle S. Root <kroot27@gmail.com>
2018-10-31 10:31:58 -0600

    Cleaned up some code

--- r_scripts/erhs_535_homework_4_script.R
+++ r_scripts/erhs_535_homework_4_script.R
@@ -8,7 +8,10 @@ homicides <- read_csv("data_raw/data-homicides-master/homicide-data.csv")
 head(homicides)
 
 homicides <- homicides %>% 
-  unite(city_name, city, state, sep = ", ")
+  unite(city_name,
+        city,
+        state,
+        sep = ", ")
 
 homicides$city_name
 
@@ -30,16 +33,22 @@ tidy(baltimore_prop) %>%
   select(estimate, conf.low, conf.high)
 
 unsolved_prop <- unsolved %>% 
-  mutate(test = map2(n_unsolved, n_homicides,  ~ prop.test(.x, n = .y))) %>% 
-  mutate(test = map(test, ~ tidy(.x))) %>% 
+  mutate(test = map2(n_unsolved, n_homicides, 
+                     ~ prop.test(.x, n = .y))) %>% 
+  mutate(test = map(test,
+                    ~ tidy(.x))) %>% 
   unnest(.drop = TRUE) %>% 
-  select(city_name, estimate, conf.low, conf.high)
+  select(city_name,
+         estimate,
+         conf.low,
+         conf.high)
   
 head(unsolved_prop)
 
 unsolved_prop %>% 
   mutate(city_name = fct_reorder(city_name, estimate)) %>% 
-  ggplot(aes(x = city_name, y = estimate)) +
+  ggplot(aes(x = city_name,
+             y = estimate)) +
   geom_point(color = "white") +
   geom_errorbar(color = "white", aes(x = city_name,
                                      ymin = conf.low,
